bf70f9505d1909c63dfa17bdb999410caec6a95c
Ahmed yussuf Abdullahi <ahmedyussuf917@gmail.com>
2023-11-29 13:01:35 +0300

    completed

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,15 @@
-#!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self):
+        self.items = []
+        self.total = 0.0
+
+    def add_item(self, item_name, item_price, quantity=1):
+        self.items.extend([(item_name, item_price, quantity)])
+        self.total += item_price * quantity
+
+    def void_last_transaction(self):
+        if self.items:
+            _, last_item_price, last_item_quantity = self.items.pop()
+            self.total -= last_item_price * last_item_quantity
+
+    pass
